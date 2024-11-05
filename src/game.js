37bf181ab232e11139f7bfc440cc037c4b488d06
pedroleo <pedroleo@ucm.es>
2024-11-05 03:53:21 +0100

    Canvas de phaser a 16:9

---
+++ src/game.js
@@ -0,0 +1,36 @@
+const max_w = 1360, max_h = 765, min_w = 320, min_h = 240;
+
+const config = {
+    width: max_w,
+    height: max_h,
+    backgroundColor: '#000000',
+    version: "1.0",
+
+    type: Phaser.AUTO,
+    // Nota: el orden de las escenas es relevante, y las que se encuentren antes en el array se renderizaran por debajo de las siguientes
+    scene: [],
+    autoFocus: true,
+    // Desactivar que aparezca el menu de inspeccionar al hacer click derecho
+    disableContextMenu: true,
+    render: {
+        antialias: true,
+    },
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH,   // CENTER_BOTH, CENTER_HORIZONTALLY, CENTER_VERTICALLY
+        mode: Phaser.Scale.FIT,                 // ENVELOP, FIT, HEIGHT_CONTROLS_WIDTH, NONE, RESIZE, WIDTH_CONTROLS_HEIGHT
+        min: {
+            width: min_w,
+            height: min_h
+        },
+        max: {
+            width: max_w,
+            height: max_h,
+        },
+        zoom: 1,
+        parent: 'game',
+    },
+}
+
+const game = new Phaser.Game(config);
+// Propiedad debug
+game.debug = false;
