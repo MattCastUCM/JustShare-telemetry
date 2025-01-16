06e980a51f6213a2c1569f4e5369c1bf21724a24
MattCastUCM <mattcast@ucm.es>
2025-01-16 19:36:50 +0100

    Arreglo final ruta A

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
     <img src="assets/UI/logo.png"/>
   </a>
 </div>
-<br>
 
 # 
 
@@ -125,7 +124,5 @@ Para las fotos que se mandan los personajes por la aplicación de mensajería, s
 <br>
 
 ---
-<br>
-
 
 <p xmlns:cc="http://creativecommons.org/ns" >This work is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>
--- src/scenes/gameLoop/scene6/routeA/scene6EndingRouteA.js
+++ src/scenes/gameLoop/scene6/routeA/scene6EndingRouteA.js
@@ -13,23 +13,42 @@ export default class Scene6EndingRouteA extends BaseScene {
     create(params) {
         super.create(params)
 
+        
+        // Lee el archivo de nodos
+        this.nodes = this.cache.json.get('scene6EndingRouteA');
+
+        
+        // Al producirse, se cambian los dialogos de la cama y el armario
+        this.dispatcher.add("addMomChat", this, () => {
+            let chatName = this.gameManager.translate("textMessages.chat4", { ns: "deviceInfo", returnObjects: true });
+            this.phoneManager.phone.addChat(chatName, "momPfp");
+            let phoneNode = super.readNodes(this.nodes, "scene6\\routeA\\scene6EndingRouteA", "mom", true);
+            this.phoneManager.phone.setChatNode(chatName, phoneNode);
+        });
+
         // Al producirse, se cambian los dialogos de la cama y el armario
         this.dispatcher.add("end", this, () => {
             this.phoneManager.togglePhone();
             this.phoneManager.activatePhoneIcon(false);
-            
-            let params = {
-                fadeOutTime: 1000,
-            };
-            this.gameManager.changeScene("TitleScene", params);
+
+            // Una vez termina la animacion, se introduce un retardo y cuando acaba,
+            // se cambia a la escena de transicion y luego a la escena final
+            setTimeout(() => {
+                let params = {
+                    fadeOutTime: 1000,
+                    text: this.gameManager.translate("scene7.start", { ns: "transitions", returnObjects: true }),
+                    onComplete: () => {
+                        this.UIManager.moveLids(true);
+                        this.gameManager.changeScene("Scene7Bedroom");
+                    },
+                };
+                this.gameManager.changeScene("TextOnlyScene", params);
+            }, 1000);
         });
     }
 
     // Se hace esto porque si se establece un dialogo en la constructora, no funciona el bloqueo del fondo del DialogManager
     onCreate() {
-        // Lee el archivo de nodos
-        let nodes = this.cache.json.get('scene6EndingRouteA');
-        
         setTimeout(() => {
             this.phoneManager.activatePhoneIcon(true);
             this.phoneManager.togglePhone();
@@ -39,17 +58,9 @@ export default class Scene6EndingRouteA extends BaseScene {
             setTimeout(() => {
                 let chatName = this.gameManager.translate("textMessages.chat3", { ns: "deviceInfo", returnObjects: true });
                 this.phoneManager.phone.addChat(chatName, "dadPfp");
-                let phoneNode = super.readNodes(nodes, "scene6\\routeA\\scene6EndingRouteA", "dad", true);
+                let phoneNode = super.readNodes(this.nodes, "scene6\\routeA\\scene6EndingRouteA", "dad", true);
                 this.phoneManager.phone.setChatNode(chatName, phoneNode);
-            }, 2000);
-            
-            setTimeout(() => {
-                let chatName = this.gameManager.translate("textMessages.chat4", { ns: "deviceInfo", returnObjects: true });
-                this.phoneManager.phone.addChat(chatName, "momPfp");
-                let phoneNode = super.readNodes(nodes, "scene6\\routeA\\scene6EndingRouteA", "mom", true);
-                this.phoneManager.phone.setChatNode(chatName, phoneNode);
-            }, 7000);
-
+            }, 1000);
         }, 1000);
     }
 }
