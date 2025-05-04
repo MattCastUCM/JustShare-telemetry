09006839a218f7cef2a429377750e62d38bf9a5f
María Sachez <marsache@ucm.es>
2025-05-04 16:51:06 +0200

    Accessible & Alternative

---
+++ src/tracker/accessible.js
@@ -0,0 +1,28 @@
+export default class Accessible {
+    constructor(tracker) {
+        this.tracker = tracker;
+        this.types = {
+            screen: 'https://w3id.org/xapi/seriousgames/activity-types/screen' ,
+            area: 'https://w3id.org/xapi/seriousgames/activity-types/area',
+            zone: 'https://w3id.org/xapi/seriousgames/activity-types/zone',
+            cutscene: 'https://w3id.org/xapi/seriousgames/activity-types/cutscene',
+            accessible: 'https://w3id.org/xapi/seriousgames/activity-types/accessible'
+        };
+    }
+
+    accessed(id, type = 'Accessible') {
+        this.tracker.addEvent({
+            verb: 'https://w3id.org/xapi/seriousgames/verbs/accessed',
+            objectType: this.types[type],
+            id: id
+        });
+    }
+
+    skipped(id, type = 'Accessible') {
+        this.tracker.addEvent({
+            verb: 'http://id.tincanapi.com/verb/skipped',
+            objectType: this.types[type],
+            id: id
+        });
+    }
+}
---
+++ src/tracker/alternative.js
@@ -0,0 +1,29 @@
+export default class Alternative {
+    constructor(tracker) {
+        this.tracker = tracker;
+        this.types = {
+            question: 'http://adlnet.gov/expapi/activities/question' ,
+            menu: 'https://w3id.org/xapi/seriousgames/activity-types/menu',
+            dialog: 'https://w3id.org/xapi/seriousgames/activity-types/dialog-tree',
+            path: 'https://w3id.org/xapi/seriousgames/activity-types/path',
+            arena: 'https://w3id.org/xapi/seriousgames/activity-types/arena',
+            alternative: 'https://w3id.org/xapi/seriousgames/activity-types/alternative'
+        };
+    }
+
+    selected(id, type = 'Alternative') {
+        this.tracker.addEvent({
+            verb: 'https://w3id.org/xapi/adb/verbs/selected',
+            objectType: this.types[type],
+            id: id
+        });
+    }
+
+    unlocked(id, type = 'Alternative') {
+        this.tracker.addEvent({
+            verb: 'https://w3id.org/xapi/seriousgames/verbs/unlocked',
+            objectType: this.types[type],
+            id: id
+        });
+    }
+}
