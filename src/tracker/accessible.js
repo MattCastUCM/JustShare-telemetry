638205e96a5652b94248de7e2b7e8c79b177d424
María Sachez <marsache@ucm.es>
2025-05-08 09:59:09 +0200

    Accessible & Alternative

--- src/tracker/accessible.js
+++ src/tracker/accessible.js
@@ -1,28 +1,59 @@
 export default class Accessible {
     constructor(tracker) {
         this.tracker = tracker;
+
         this.types = {
-            screen: 'https://w3id.org/xapi/seriousgames/activity-types/screen',
-            area: 'https://w3id.org/xapi/seriousgames/activity-types/area',
-            zone: 'https://w3id.org/xapi/seriousgames/activity-types/zone',
-            cutscene: 'https://w3id.org/xapi/seriousgames/activity-types/cutscene',
-            accessible: 'https://w3id.org/xapi/seriousgames/activity-types/accessible'
+            screen: 0,
+            area = 1,
+            zone = 2,
+            cutscene = 3,
+            properties: ["screen", "area", "zone", "cutscene"]
+        }
+
+        this.ids = {
+            "screen": 'https://w3id.org/xapi/seriousgames/activity-types/screen',
+            "area": 'https://w3id.org/xapi/seriousgames/activity-types/area',
+            "zone": 'https://w3id.org/xapi/seriousgames/activity-types/zone',
+            "cutscene": 'https://w3id.org/xapi/seriousgames/activity-types/cutscene'
         };
+
+        this.descriptions = {
+            "screen": 'A game screen where a concrete game task is developed. Games are usually composed by a set of screens the player navigates. A screen has a concrete function within the game. A screen contains a menu, or the gameplay. To define virtual zones inside the game world, it is recommended to use area or zone.',
+            "area": 'An identified area inside the game world. In some games they can also be worlds. Represents an aggregation of zones.',
+            "zone": 'An identified zone inside an area of the game world. A zone is inside an area, and defines a logic self-contained place.',
+            "cutscene": 'A non-interactive scene that can be skipped. A cutscene is a non-interactive scene within the game (e.g., a video, a conversation, an automated sequence). The player can usually skip them pressing some button.'
+        }
     }
 
-    accessed(id, type = 'Accessible') {
+    accessed(type, accessibleName) {
+        let property = this.types.properties[type]
         this.tracker.addEvent({
-            verb: 'https://w3id.org/xapi/seriousgames/verbs/accessed',
-            objectType: this.types[type],
-            id: id
+            verb: {
+                id: 'https://w3id.org/xapi/seriousgames/verbs/accessed',
+                type: "accessed"
+            },
+            object: {
+                id: this.ids[property],
+                type: property,
+                description: this.descriptions[property],
+                name: accessibleName
+            }
         });
     }
 
-    skipped(id, type = 'Accessible') {
+    skipped(type, accessibleName) {
+        let property = this.types.properties[type]
         this.tracker.addEvent({
-            verb: 'http://id.tincanapi.com/verb/skipped',
-            objectType: this.types[type],
-            id: id
+            verb: {
+                id: 'http://id.tincanapi.com/verb/skipped',
+                type: "skipped"
+            },
+            object: {
+                id: this.ids[property],
+                type: property,
+                description: this.descriptions[property],
+                name: accessibleName
+            }
         });
     }
 }
--- src/tracker/alternative.js
+++ src/tracker/alternative.js
@@ -1,29 +1,58 @@
 export default class Alternative {
     constructor(tracker) {
         this.tracker = tracker;
+
         this.types = {
-            question: 'http://adlnet.gov/expapi/activities/question',
-            menu: 'https://w3id.org/xapi/seriousgames/activity-types/menu',
-            dialog: 'https://w3id.org/xapi/seriousgames/activity-types/dialog-tree',
-            path: 'https://w3id.org/xapi/seriousgames/activity-types/path',
-            arena: 'https://w3id.org/xapi/seriousgames/activity-types/arena',
-            alternative: 'https://w3id.org/xapi/seriousgames/activity-types/alternative'
+            question = 0,
+            menu = 1,
+            dialog = 2,
+            path = 3,
+            arena = 4,
+            properties: ["question", "menu", "dialog", "path", "arena"]
+        }
+
+        this.ids = {
+            "question": 'http://adlnet.gov/expapi/activities/question',
+            "menu": 'https://w3id.org/xapi/seriousgames/activity-types/menu',
+            "dialog-tree": 'https://w3id.org/xapi/seriousgames/activity-types/dialog-tree'
+        };
+
+        this.descriptions = {
+            "question": 'A question is typically part of an assessment and requires a response from the learner, a response that is then evaluated for correctness.',
+            "menu": 'A menu with several buttons/options whose selection produces different effects. Represents a menu, integrated in the user interface (UI), with several a options.',
+            "dialog-tree": 'An alternative presented during a conversation with an non-playable character. Dialog trees are a common mechanic in video games, where players can answer questions to NPC. This activity represents that type of alternative.'
         };
     }
 
-    selected(id, type = 'Alternative') {
+    selected(type, alternativeName) {
+        let property = this.types.properties[type]
         this.tracker.addEvent({
-            verb: 'https://w3id.org/xapi/adb/verbs/selected',
-            objectType: this.types[type],
-            id: id
+            verb: {
+                id: 'https://w3id.org/xapi/adb/verbs/selected',
+                type: "selected"
+            },
+            object: {
+                id: this.ids[property],
+                type: property,
+                description: this.descriptions[property],
+                name: alternativeName
+            }
         });
     }
 
-    unlocked(id, type = 'Alternative') {
+    unlocked(type, alternativeName) {
+        let property = this.types.properties[type]
         this.tracker.addEvent({
-            verb: 'https://w3id.org/xapi/seriousgames/verbs/unlocked',
-            objectType: this.types[type],
-            id: id
+            verb: {
+                id: 'https://w3id.org/xapi/seriousgames/verbs/unlocked',
+                type: "unlocked"
+            },
+            object: {
+                id: this.ids[property],
+                type: property,
+                description: this.descriptions[property],
+                name: alternativeName
+            }
         });
     }
 }
