3c593021850805cc64efde1741a7b88bbb4d9cbe
likang03 <likangwu@ucm.es>
2025-05-11 00:12:58 +0200

    Update trackerScene.js

--- src/tracker/trackerScene.js
+++ src/tracker/trackerScene.js
@@ -10,32 +10,32 @@ export default class TrackerScene extends Phaser.Scene {
     }
 
     create() {
-        // var tracker = new Tracker(
-        //     new LRS({
-        //         baseUrl: "https://cloud.scorm.com/lrs/I43WO3TFWH/sandbox/",
-        //         authScheme: new BasicAuthentication("oMsoz51hM_OQbNNR3Nk", "LfWapsOhe1V-ryV2C6o")
-        //     }),
-        //     new AccountActor("http://example.com", "holita")
-        // );
-
         var tracker = new Tracker(
             new LRS({
-                baseUrl: "https://simva-api.simva-beta.e-ucm.es/activities/681b46a0fbe4c00023c56cc4/",
-                authScheme: new OAuth2({
-                    tokenEndpoint: "https://sso.simva-beta.e-ucm.es/realms/simva/protocol/openid-connect/token",
-                    grantType: "password",
-                    clientId: "simva-plugin",
-                    password: "slpa",
-                    username: "slpa",
-                    loginHint: "6818e70cfbe4c00023c50ab4"
-                }),
-                backup: {
-                    endpoint: "https://simva-api.simva-beta.e-ucm.es:443/activities/681b46a0fbe4c00023c56cc4/result",
-                    type: "XAPI"
-                }
+                baseUrl: "https://cloud.scorm.com/lrs/I43WO3TFWH/sandbox/",
+                authScheme: new BasicAuthentication("oMsoz51hM_OQbNNR3Nk", "LfWapsOhe1V-ryV2C6o")
             }),
             new AccountActor("http://example.com", "holita")
-        )
+        );
+
+        // var tracker = new Tracker(
+        //     new LRS({
+        //         baseUrl: "https://simva-api.simva-beta.e-ucm.es/activities/681b46a0fbe4c00023c56cc4/",
+        //         authScheme: new OAuth2({
+        //             tokenEndpoint: "https://sso.simva-beta.e-ucm.es/realms/simva/protocol/openid-connect/token",
+        //             grantType: "password",
+        //             clientId: "simva-plugin",
+        //             password: "slpa",
+        //             username: "slpa",
+        //             loginHint: "6818e70cfbe4c00023c50ab4"
+        //         }),
+        //         backup: {
+        //             endpoint: "https://simva-api.simva-beta.e-ucm.es:443/activities/681b46a0fbe4c00023c56cc4/result",
+        //             type: "XAPI"
+        //         }
+        //     }),
+        //     new AccountActor("http://example.com", "holita")
+        // )
 
         // var tracker = generateTrackerFromURL()
 
@@ -45,9 +45,16 @@ export default class TrackerScene extends Phaser.Scene {
         var alternative = tracker.alternative
 
         completable.initialized(completable.types.area, "Hola")
+        completable.initialized(completable.types.area, "Hola2")
+        completable.initialized(completable.types.area, "Hola3")
+        completable.initialized(completable.types.area, "Hola4")
+        completable.initialized(completable.types.area, "Hola5")
+        completable.initialized(completable.types.area, "Hola6")
+        completable.initialized(completable.types.area, "Hola7")
 
-        setTimeout(() => {
-            tracker.lrs.logout()
-        }, 2000);
+
+        // setTimeout(() => {
+        //     tracker.lrs.logout()
+        // }, 2000);
     }
 }
