f76d704b4c6745c3a6776ced6aa03af83066911b
NanxiQin <krystal12041994@gmail.com>
2025-05-17 17:50:45 +0200

    mas funcionalidad de utils

--- python/utils.py
+++ python/utils.py
@@ -4,13 +4,21 @@ import pandas as pd
 def find_indices_by_conditions(df, conditions):
     mask = pd.Series([True] * len(df), index=df.index)
     for col, val in conditions:
-        mask &= df[col] == val
+        if val=="notna":
+            mask &= df[col].notna()
+        else:
+            mask &= df[col] == val
     return df[mask].index.tolist()
 
 # Dada una serie de condiciones (por ejemplo, objeto de tipo Game) obtener el primer indice en el dataframe
 def find_first_index_by_conditions(df, conditions):
     indices = find_indices_by_conditions(df, conditions)
     return indices[0] if indices else None
+
+# Devuelve el valor de una columna específica en la primera fila que cumple las condiciones
+def find_first_value_by_conditions(df, conditions, column):
+    indices = find_indices_by_conditions(df, conditions)
+    return df.loc[indices[0], column] if indices else None
 
 # Dado dos indices obtener la diferencia de tiempos
 def time_between_indices(df, index1, index2):
